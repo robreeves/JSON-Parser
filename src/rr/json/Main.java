603452cd3d3565c6811db4a3739fe144508e0975
robreeves <robert.p.reeves@gmail.com>
2016-05-15 13:20:27 -0700

    github spacing fix

--- src/rr/json/Main.java
+++ src/rr/json/Main.java
@@ -3,7 +3,7 @@ package rr.json;
 public class Main {
 
     public static void main(String[] args) {
-	    if (args.length == 1) {
+        if (args.length == 1) {
             JsonLexer lexer = new JsonLexer(args[0]);
             JsonParser parser = new JsonParser(lexer);
 
